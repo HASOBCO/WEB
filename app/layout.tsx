0a1783740a6ef518de0807cd4dacd148a4a068cc
HASOBCO <153663183+HASOBCO@users.noreply.github.com>
2023-12-12 19:40:06 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,6 @@ export const metadata: Metadata = {
   title: 'Hasob - Development of the Arab world',
   description: 'Leading innovation and progress in the Arab region through technology and expertise.',
   keywords: 'حاسوب, HASOB CO, تطوير العالم العربي, Arab world development, مبرمجين عرب, Arab programmers, تقنية, technology, برمجة, programming, ابتكار, innovation, تطور, progress, حاسوب للمستقبل, future of Hasob, برمجة عربية, Arab programming, تكنولوجيا المعلومات, information technology, الثورة التكنولوجية, technological revolution, حلول تكنولوجية, technological solutions, حاسوب وبرمجة, Hasob in programming, البرمجة في العالم العربي, programming in the Arab world',
-  author: 'Hasob',
 };
 
 export default function RootLayout({
