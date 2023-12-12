a5cd2134fec0274afe3655d3c92f73872f7af806
root <root@HNODE-1.local>
2023-12-12 19:33:34 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  images: {
+    remotePatterns: [
+        { hostname: 'i.ibb.co' },
+    ],
+},
+    optimizeFonts: false, // Keep this as is if you need to disable font optimization
+};
+
+module.exports = nextConfig;
